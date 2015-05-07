5de94a8c945211171d2978cbd55615ba5b0ed8f2
Satit Rianpit <rianpit@gmail.com>
2015-05-07 13:43:17 +0700

    add init function

--- bin/hdc-sync.js
+++ bin/hdc-sync.js
@@ -1,3 +1,30 @@
+#!/usr/bin/env node
 
+'use strict';
+
+// Get argvs;
 var argv = require('minimist')(process.argv.slice(2));
-console.dir(argv);
+// Main Sync program
+var Sync = require('../lib/hdc-sync');
+
+//console.dir(argv);
+
+// Show help
+if (argv.help) {
+
+}
+
+// Set display log to console
+if (argv.console) {
+
+}
+
+// Test connection
+if (argv.test) {
+    Sync.testConnection();
+}
+
+// Running server mode
+if (argv.server) {
+    console.log('Listening on port: 3333');
+}
